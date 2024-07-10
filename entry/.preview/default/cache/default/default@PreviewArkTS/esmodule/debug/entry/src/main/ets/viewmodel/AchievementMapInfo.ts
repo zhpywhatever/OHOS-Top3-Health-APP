9a9f5465daf24a6513801a49b9b982077ac53764
Chen Jinyuan <3220105455@zju.edu.cn>
2024-07-10 16:00:17 +0800

    Feature: Solve toast

---
+++ entry/.preview/default/cache/default/default@PreviewArkTS/esmodule/debug/entry/src/main/ets/viewmodel/AchievementMapInfo.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Achievement map info
+ */
+export default class AchievementMapInfo {
+    off_3: Resource = { "id": 16777393, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
+    on_3: Resource = { "id": 16777231, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
+    off_7: Resource = { "id": 16777359, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
+    on_7: Resource = { "id": 16777348, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
+    off_30: Resource = { "id": 16777347, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
+    on_30: Resource = { "id": 16777339, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
+    off_50: Resource = { "id": 16777277, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
+    on_50: Resource = { "id": 16777335, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
+    off_73: Resource = { "id": 16777400, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
+    on_73: Resource = { "id": 16777399, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
+    off_99: Resource = { "id": 16777388, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
+    on_99: Resource = { "id": 16777344, "type": 20000, params: [], "bundleName": "com.example.healthy_life", "moduleName": "entry" };
+}
